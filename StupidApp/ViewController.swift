1967e23d93e53cc590c5726423217f944b73a117
Sydothekido11 <sydnemelka@gmail.com>
2015-12-15 12:35:05 -0700

    Added a color change.

--- StupidApp/ViewController.swift
+++ StupidApp/ViewController.swift
@@ -11,6 +11,16 @@ import UIKit
 class ViewController: UIViewController
 {
 
+    @IBAction func changeBackgroundColor
+        (sender: UIButton)
+    {
+        
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
     override func viewDidLoad()
     {
         super.viewDidLoad()
